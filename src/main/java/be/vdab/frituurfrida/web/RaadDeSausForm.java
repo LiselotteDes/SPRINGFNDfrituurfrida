e11d51eb23fbfadfabdea3bc29efd7eefbcfb0c8
Liselotte <liselotte.des@gmail.com>
2018-03-19 08:52:14 +0100

    20: Saus raden (private variabele van RaadDeSausForm primitief gemaakt)

--- src/main/java/be/vdab/frituurfrida/web/RaadDeSausForm.java
+++ src/main/java/be/vdab/frituurfrida/web/RaadDeSausForm.java
@@ -1,13 +1,19 @@
 package be.vdab.frituurfrida.web;
 
 class RaadDeSausForm {
-	private char letter = ' ';
+	/*
+	 * Eerst had ik char als type: werkt ook.
+	 * Waarom Character? Omdat char niet op null kan staan. Heeft altijd een beginwaarde (zoals int 0 is): '\u0000'.
+	 * Dit effect zie je als je eerst naar de website gaat: er staat een vraagtekentje in het vak.
+	 * Dit kan je vermijden door een wrappertype te nemen voor je formulieren !
+	 */
+	private Character letter = ' ';
 
-	public char getLetter() {
+	public Character getLetter() {
 		return letter;
 	}
 
-	public void setLetter(char letter) {
+	public void setLetter(Character letter) {
 		this.letter = letter;
 	}
 }
